aae7823e511a3131424d841492da5669c0c82e26
zexcov102 <bje69708@gmail.com>
2026-05-08 01:56:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-const {
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bot is running');
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Web server running');
+});const {
   Client,
   GatewayIntentBits,
   SlashCommandBuilder,
